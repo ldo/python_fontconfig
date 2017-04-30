8547fcd49f5305a4ea483af323012b4a85719199
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-30 22:03:12 +0000

    add Python version check

--- setup.py
+++ setup.py
@@ -8,7 +8,25 @@
 # Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 #-
 
+import sys
 import distutils.core
+from distutils.command.build import \
+    build as std_build
+
+class my_build(std_build) :
+    "customization of build to perform additional validation."
+
+    def run(self) :
+        try :
+            import enum
+        except ImportError :
+            sys.stderr.write("This module requires Python 3.4 or later.\n")
+            sys.exit(-1)
+        #end try
+        super().run()
+    #end run
+
+#end my_build
 
 distutils.core.setup \
   (
@@ -20,4 +38,8 @@ distutils.core.setup \
     author_email = "ldo@geek-central.gen.nz",
     url = "http://github.com/ldo/python_fontconfig",
     py_modules = ["fontconfig"],
+    cmdclass =
+        {
+            "build" : my_build,
+        },
   )
